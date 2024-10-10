f8b38d7d95b1a4de11857b92597698aaea373adc
alishaayyyy <alisha12740@gmail.com>
2024-10-10 00:54:29 -0700

    done

---
+++ app.js
@@ -0,0 +1,48 @@
+
+// 1
+var var1 = 20, var2 = 30, var3 = 30;
+console.log(var1 + var2 + var3);
+
+//  legal variable
+
+// myVar
+// $dollar
+// _doit
+// firstName
+// MyClass
+
+// illegal variable
+
+
+// break
+// @UserName
+// My-Work
+// my age
+// var
+
+var num = "numbers,";
+var $sign = "$";
+var _sign = "_";
+var example = "$my_1stVariable";
+var letter = "letter,";
+var example2 = "$name,_name";
+var name = "name";
+var Case = "case sensitive";
+var key = "key words";
+
+document.write('<h2> "Rules for naming JS variables" </h2>' );
+
+document.write("Variable names can only contain" + " " + num + " " + $sign + "and " + " " + _sign +"." +" For example:" + " " + example );
+document.write('<br>');
+document.write("Variable must begin with a " + " " + letter + " " + $sign + " " +" or" + " " + _sign + " " + "For example " + " " + example2 +" " + "or " + " name" );
+document.write('<br>');
+document.write("Variable names are " + " " + Case);
+document.write('<br>');
+document.write("Variables names should not be JS" + " " + key)
+
+
+
+
+
+
+
